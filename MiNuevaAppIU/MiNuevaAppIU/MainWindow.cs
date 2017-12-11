9fe5af7207c545584bda0b575420940e8cbfd3a8
katho <katarino240sx@gmail.com>
2017-12-11 17:09:51 -0600

    Agregar el resto de los achivos

---
+++ MiNuevaAppIU/MiNuevaAppIU/MainWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnButton3Clicked (object sender, EventArgs e)
+	{
+		Console.WriteLine ("Botón!!!");
+		MessageDialog md = new MessageDialog (this, DialogFlags.DestroyWithParent,
+			MessageType.Info,
+			ButtonsType.Ok, "HelloWorld!!!");
+		md.Run ();
+		md.Destroy();
+	}
+
+	protected void OnButton4Clicked (object sender, EventArgs e)
+	{
+		Console.WriteLine ("Botón!!!");
+		MessageDialog md = new MessageDialog (this, DialogFlags.DestroyWithParent,
+			MessageType.Info,
+			ButtonsType.Ok, "HelloWorld!!!");
+		md.Run ();
+		md.Destroy();
+	}
+}
